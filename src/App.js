dabe26ada20e3e59940b264592948741d89c56fe
Max <maxk.zi777@gmail.com>
2021-09-06 17:39:14 +0300

    change date

--- src/App.js
+++ src/App.js
@@ -1,53 +1,52 @@
-import './App.css';
+import './App.css'
 import CountdownHeader from './Components/CountdownHeader'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import CountdownBody from './Components/CountdownBody'
 
 function App() {
-  const [today] = useState({date: new Date()})
-  const [timeLeft, setTimeLeft] = useState({
-    days: null,
-    hours: null,
-    minutes: null,
-    seconds: null
-  })
-  
-  function time(param) {
-    return param < 10 ? `0${param}` : param
-  }
-  
-  function calcTime() {
-    const date = new Date()
-    const birthDay = new Date(`12 Feb 2022`)
-    const diffTime = birthDay - date
-    let _sec = 1000;
-    let _min = 60 * _sec;
-    let _hour = 60 * _min;
-    let _day = 24 * _hour;
-  
-    setTimeLeft({
-      days: Math.floor(diffTime / _day),
-      hours: time(Math.floor((diffTime % _day) / _hour)),
-      minutes: time(Math.floor((diffTime % _hour) / _min)),
-      seconds: time(Math.floor((diffTime % _min) / _sec))
-    });
-  }
-  
-  useEffect(() => {
-      setInterval(calcTime, 1000)
-  })
-  
-  return (
-    <div className='global__container'>
-      <CountdownHeader today={today}/>
-      <CountdownBody
-        days={timeLeft.days}
-        hours={timeLeft.hours}
-        minutes={timeLeft.minutes}
-        seconds={timeLeft.seconds}/>
-      <div className="overlay"/>
-    </div>
-  )
+	const [today] = useState({ date: new Date() })
+	const [timeLeft, setTimeLeft] = useState({
+		days: null,
+		hours: null,
+		minutes: null,
+		seconds: null,
+	})
+
+	const time = param => (param < 10 ? `0${param}` : param)
+
+	function calcTime() {
+		const date = new Date()
+		const birthDay = new Date(`01 Jan 2022`)
+		const diffTime = birthDay - date
+		let _sec = 1000
+		let _min = 60 * _sec
+		let _hour = 60 * _min
+		let _day = 24 * _hour
+
+		setTimeLeft({
+			days: Math.floor(diffTime / _day),
+			hours: time(Math.floor((diffTime % _day) / _hour)),
+			minutes: time(Math.floor((diffTime % _hour) / _min)),
+			seconds: time(Math.floor((diffTime % _min) / _sec)),
+		})
+	}
+
+	useEffect(() => {
+		setInterval(calcTime, 1000)
+	})
+
+	return (
+		<div className='global__container'>
+			<CountdownHeader today={today} />
+			<CountdownBody
+				days={timeLeft.days}
+				hours={timeLeft.hours}
+				minutes={timeLeft.minutes}
+				seconds={timeLeft.seconds}
+			/>
+			<div className='overlay' />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/Components/CountdownHeader.jsx
+++ src/Components/CountdownHeader.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const CountdownHeader = (props) => {
+const CountdownHeader = props => {
 	return (
-		<div className="header">
-			<div className="header-text">Days to Birthday</div>
-			<span className="header-date">{props.today.date.toDateString()} - today</span>
+		<div className='header'>
+			<div className='header-text'>Days to New Year</div>
+			<span className='header-date'>{props.today.date.toDateString()} - today</span>
 		</div>
 	)
 }
